11d2c35d9ebf26270c3bc3c587e37bf0013204fe
Giannis Giannopoulos <johngiannop@gmail.com>
2019-09-07 15:51:11 +0300

    Update SmartTables.php

--- SmartTables.php
+++ SmartTables.php
@@ -19,7 +19,7 @@ Class SmartTables {
         }
         return $inst;
     }
-    public function GetTable(){
+    public function GetTable($options=null){
         $columns = array();
         $searchablecolumns = array();
         $cols=$this->requestData["columns"];
@@ -31,44 +31,76 @@ Class SmartTables {
                 array_push($searchablecolumns ,$col);
             }
         }
-        $st = $this->db->prepare("SELECT * FROM $this->table");
+        $addsql="";
+        $addsqlwhere="";
+        if($options!=null){
+            if(isset($options["join"])){
+                foreach($options["join"] as $join){
+                    $addsql.=" ".$join[1]." join ".$join[0]." on ".$join[0].".".$join[2][1]." = ".$this->table.".".$join[2][0];
+                }
+            }
+            if(isset($options["where"])){
+               
+                $addsqlwhere.=" where ";
+                foreach($options["where"] as $where){
+                    $addsqlwhere.=$where[0].$where[1].$where[2] ." and";
+                }
+             
+                $addsqlwhere=rtrim($addsqlwhere," and");
+            }
+        }
+       
+        $sql="SELECT count(*) as total FROM ". $this->table . $addsql. $addsqlwhere ;
+        $st= $this->db->prepare($sql);
         $st->execute();
-        $totalData = $st->rowCount();
+        $totalData = $st->fetch(PDO::FETCH_ASSOC)["total"];
+
         $totalDraw = $totalData;
         $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
         $len=$this->requestData['length']=="All"?"":" LIMIT " . $this->requestData['start'] . " ," . $this->requestData['length'];
 
         if (!empty($this->requestData['search']['value'])) {
             // if there is a search parameter
-            $sql = "SELECT  * FROM $this->table";
-            $sqlpat="";
+            $sql="SELECT * FROM ". $this->table . $addsql;
+           
             if(count($searchablecolumns)>0){
+                $sqlpat="";
                 $sqlpat.=" where (";
-            }
-            foreach ($searchablecolumns as $col){
+                foreach ($searchablecolumns as $col){
                 
-                if($col["searchable"]){
-                    if(isset($col["field"])){
-                         $sqlpat.=$col["field"]. " LIKE '%" . $this->requestData['search']['value'] . "%' OR ";    
+                    if($col["searchable"]){
+                        if(isset($col["field"])){
+                             $sqlpat.=$col["field"]. " LIKE '%" . $this->requestData['search']['value'] . "%' OR ";    
+                        }
                     }
                 }
+                $sqlpat=  rtrim($sqlpat,"OR ");
+                if(count($searchablecolumns)>0){
+                    $sqlpat.=" ) ";
+                }
+                $sql.=$sqlpat;
+                // if($addsqlwhere!=""){
+                //     $addsqlwhere=ltrim($addsqlwhere);
+                //     $addsqlwhere=ltrim($addsqlwhere,"where");
+                // }
+                // $sql.=" and ".$addsqlwhere;
             }
-            $sqlpat=  rtrim($sqlpat,"OR ");
-            if(count($searchablecolumns)>0){
-                $sqlpat.=" ) ";
-            }
-            $sql.=$sqlpat;
+            else{
+                $sql.=$addsqlwhere;
+            }         
+           
             $sql.=" ORDER BY " . $columns[$this->requestData['order']['column']] . "   " . $this->requestData['order']['dir'] . $len . "   ";
             $st = $this->db->prepare($sql);
             $st->execute();
             $totalDraw = $st->rowCount();
-            $sql2 = "SELECT count(*) as total from $this->table $sqlpat";
+            $sql2="SELECT count(*) as total FROM ". $this->table . $addsql .$sqlpat;
             $st2= $this->db->prepare($sql2);
             $st2->execute();
             $row2 = $st2->fetch(PDO::FETCH_ASSOC);
             $totalFiltered = $row2["total"];
         } else {
-            $sql = "SELECT  * FROM $this->table";
+            
+            $sql="SELECT * FROM ". $this->table . $addsql. $addsqlwhere;
             $sql.=" ORDER BY " . $columns[$this->requestData['order']['column']] . "   " . $this->requestData['order']['dir'] . $len . "   ";
             $st = $this->db->prepare($sql);
             $st->execute();
